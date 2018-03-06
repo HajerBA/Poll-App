13f60c3df202ce1e6890ffb8c06011e34ae9fe2b
HajerBA <hajerbnch@gmail.com>
2018-03-06 12:20:35 +0100

    updates

---
+++ src/components/answer.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="maclasse">
+   <form @submit.prevent="onSubmit()">
+  <h2>poll.title</h2>
+  <div
+  v-for="(option, index) in poll.options" 
+  :key="index"  class="form-group">
+    <input type="radio" 
+    value="option.text"
+    v-model="poll.checked"
+    required>
+  <input type="submit">
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+    name:'answer',
+  data () {
+    return {
+      poll: {
+      title: "Que boire au petit dej ?",
+      options: [
+        {text: "Thé", checked: false},
+        {text: "Café", checked: false},
+        {text: "Jus d'orange", checked: false},
+        {text: "Rien, je suis en retard", checked: false},
+  ]
+}
+    }
+  },
+  
+    methods: {
+    onSubmit(){
+      
+        console.log(this.poll.options)
+      
+      
+     }
+    }
+}
+</script>
+<style>
+.maclasse{
+  border: 1px solid;
+  margin: auto;
+  width: 400px;
+}
+</style>
